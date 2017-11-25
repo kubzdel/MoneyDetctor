0b93070e0d60b3167d932878e33b9e6b70e65a36
Jakub Tomczak <jakub.tomczak@live.com>
2017-11-25 20:11:59 +0100

    Silver coin detector.

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from matplotlib.pyplot import figure, subplot
 from skimage import data as img
 from skimage import filters
 from matplotlib import pyplot as plt
-from skimage.color import rgb2gray
+from skimage.color import rgb2gray, label2rgb
 from skimage.feature import ORB, match_descriptors
+from skimage.filters import threshold_otsu
 from skimage.measure import label, regionprops, find_contours
-from skimage.morphology import closing, disk
+from skimage.morphology import closing, disk, square
 from skimage.draw import line
 from skimage.segmentation import clear_border
 import skimage.morphology as mp
@@ -32,6 +33,82 @@ import cv2
 import PIL
 from IPython.display import display, Image
 
+def method2(oryg,image):
+
+    thresh = threshold_otsu(image)
+
+    bw = closing(image > thresh, square(3))
+    drawPlot(bw)
+    # remove artifacts connected to image border
+    cleared = clear_border(bw)
+
+    # label image regions
+    label_image = label(cleared)
+    image_label_overlay = label2rgb(label_image, image=oryg)
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.imshow(image_label_overlay)
+
+    for region in regionprops(label_image):
+        # take regions with large enough areas
+        if region.area >= 100:
+            # draw rectangle around segmented coins
+            minr, minc, maxr, maxc = region.bbox
+            rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                      fill=False, edgecolor='red', linewidth=2)
+            ax.add_patch(rect)
+
+    ax.set_axis_off()
+    plt.tight_layout()
+    plt.show()
+
+def extractRGB(img):
+    R = np.zeros(img.shape)
+    R[:,:,:] = [1,0,0]
+    G = np.zeros(img.shape)
+    G[:,:,:] = [0,1,0]
+    B = np.zeros(img.shape)
+    B[:,:,:] = [0,0,1]
+    R = img*R
+    G = img*G
+    B = img*B
+    #gray = rgb2gray(img)*255
+    #R = np.maximum(0, gray - rgb2gray(img*G))
+    #G = np.maximum(0, gray, rgb2gray(img*G))
+    #B = np.maximum(0, gray - rgb2gray(img*B))
+    return R,G,B
+
+def color_distance(image):
+    from skimage import io, color, img_as_float
+    from skimage.feature import corner_peaks, plot_matches
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    image = img_as_float(image)
+
+    black_mask = color.rgb2gray(image) < 0.1
+    distance_5gr = color.rgb2gray(1 - np.abs(image - (.83, .76, .59)))
+    distance_1zl = color.rgb2gray(1 - np.abs(image - (0, 0, 1)))
+
+    distance_5gr[black_mask] = 0
+    distance_1zl[black_mask] = 0
+
+    coords_red = corner_peaks(distance_5gr, threshold_rel=0.9, min_distance=50)
+    coords_blue = corner_peaks(distance_1zl, threshold_rel=0.9, min_distance=50)
+
+    f, ((ax0, ax1), (ax2, ax3)) = plt.subplots(2, 2, figsize=(15, 10))
+    ax0.imshow(image)
+    ax0.set_title('Input image')
+    ax1.imshow(image)
+    ax1.set_title('Marker locations')
+    ax1.plot(coords_red[:, 1], coords_red[:, 0], 'ro')
+    ax1.plot(coords_blue[:, 1], coords_blue[:, 0], 'bo')
+    ax1.axis('image')
+    ax2.imshow(distance_5gr, interpolation='nearest', cmap='gray')
+    ax2.set_title("{dist}_{std}_{mid}".format(dist = np.mean(distance_5gr), std = np.std(distance_5gr), mid = np.median(distance_5gr)))
+    ax3.imshow(distance_1zl, interpolation='nearest', cmap='gray')
+    ax3.set_title('Distance to pure blue')
+    plt.show()
 
 def drawPlot(image):
     fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(10,10))
@@ -61,7 +138,6 @@ def circle_detector(x, y, num):
     return ("%.2f" % (100 * np.std(r) / np.mean(r)), ( np.std(r) / np.mean(r)))
 
 def thresh(t):
-
     binary = skimage.morphology.dilation(edges)
     binary = label(binary)
     #drawPlot(binary)
@@ -76,7 +152,7 @@ def thresh(t):
 
         # take regions with large enough areas
         if region.area >= 1300 and region.area < (.5*len(binary)*len(binary[0])):
-            fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+            #fig, ax = plt.subplots(1, 1, figsize=(10, 10))
             name  =  "fig_{q}_{w}".format(q = fig_name, w = iter)
 
             # draw rectangle around segmented coins
@@ -84,12 +160,25 @@ def thresh(t):
             #rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
             #                          fill=False, edgecolor='red', linewidth=2)
             coin = image2[minr:maxr, minc:maxc]
-            coin = filters.median()
+            #r,g,b = extractRGB(coin)
+            r = coin[:,:,0]
+            g = coin[:,:,1]
+            b = coin[:,:,2]
+            diffrences = [[np.max(element) - np.min(element) for element in row if np.max(element) - np.min(element) < 20] for row in coin]
 
-            #str = "{n};{rgb};{r};{g};{b};{val}".format(n = name, rgb = np.mean(coin), r = np.mean(coin[:,:,0]), g = np.mean(coin[:,:,1]), b = np.mean(coin[:,:,2]), val = np.mean(rgb2gray(coin)))
+            #print(name, np.max([np.median(r),np.median(g),np.median(b)]) - np.min([np.median(r), np.median(g), np.median(b)]))
+            ratio = sum(len(row) for row in diffrences) / (len(coin)*len(coin[0]))
+            if ratio > .5:
+                print(name, "jest srebrna")
+            #coin = filters.median(coin, np.ones((5,5)))
+            #extractRGB(coin)
+            #color_distance(coin)
+            #coin = label(rgb2gray(coin))
+            #image_label_overlay = label2rgb(coin, image=image2[minr:maxr, minc:maxc])
+            #drawPlot(image_label_overlay)
+            #str = "{n};{all}".format(n = name, all = np.mean(coin))
             #log.write(str + "\n")
-
-
+            iter+=1
             continue
             #coin = filters.median(rgb2gray(coin), np.ones((5,5)))
             ax.imshow(rgb2gray(coin))
@@ -149,7 +238,7 @@ plt.show()
 '''
 directory = os.getcwd()+"\\moje" + '/'
 
-log = open("out/figures.csv", "w+")
+log = open("out/figures_median_ones5.csv", "w+")
 for file in os.listdir(directory):
     # images.append(rgb2gray(img.load(directory + file)))
     #ORB
@@ -157,13 +246,22 @@ for file in os.listdir(directory):
     #image = img.load(directory + file, True)
     #detector.exec(img_template, histogram_manipulator.contrast_stretching(image[126:899, 138:871]), file)
     #img = hist.contrast_stretching(img)'''
+
+    '''
+    image = img.load(directory + file)
+    method2(image, rgb2gray(image))
+    '''
+
+
+    #standard
     image = img.load(directory+file,True)
     image2 = img.load(directory+file,False)
     edges = skimage.filters.sobel(image)
     edges = skimage.feature.canny(edges,1.2)
-    meanV = getMean("sobel_max_", edges)
+    #meanV = getMean("sobel_max_", edges)
     thresh(0.08)
     fig_name += 1
+
 log.close()
 #displaySaveImage(images)
 #plt.show()
