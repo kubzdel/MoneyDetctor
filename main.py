4fce4cf1228d07992d12032ea2d8036817095bd5
Jakub Tomczak <jakub.tomczak@live.com>
2017-11-25 15:07:06 +0100

    Save statistics to log file. Apply median filter.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 from skimage.color import rgb2gray
 from skimage.feature import ORB, match_descriptors
 from skimage.measure import label, regionprops, find_contours
-from skimage.morphology import closing
+from skimage.morphology import closing, disk
 from skimage.draw import line
 from skimage.segmentation import clear_border
 import skimage.morphology as mp
@@ -32,6 +32,12 @@ import cv2
 import PIL
 from IPython.display import display, Image
 
+
+def drawPlot(image):
+    fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(10,10))
+    ax.imshow(image, cmap=plt.cm.gray)
+    plt.show()
+
 def getMean(desc, image):
     mean = np.mean(image)
     print("{} image mean = {}".format(desc,mean))
@@ -54,75 +60,76 @@ def circle_detector(x, y, num):
     r = (x-xc)**2 + (y-yc)**2 #try to figure out radius of the circle with the middle of the xc and yc
     return ("%.2f" % (100 * np.std(r) / np.mean(r)), ( np.std(r) / np.mean(r)))
 
-
 def thresh(t):
-    # warnings.simplefilter("ignore")
-    binary = (edges > t) * 255
-    binary = np.uint8(binary)
-    #   binary[500, 500] = 100
-    #binary = skimage.morphology.erosion(skimage.morphology.dilation(binary))
-    binary = skimage.morphology.dilation(binary)
-    # plt.contour(binary)
-    #colorize(binary)
-    #binary = clear_border(binary)
-   # fill_coins = ndi.binary_fill_holes(binary)
-   # binary = skimage.color.gray2rgb(binary)
+
+    binary = skimage.morphology.dilation(edges)
     binary = label(binary)
-    #ax.imshow(image2)
+    #drawPlot(binary)
 
     regions = regionprops(binary)
-
     region_sizes = [reg.bbox for reg in regions]
     iter  = 0
-    with open("out/fig{q}.txt".format(q = fig_name), "w") as text_file:
-        plt.tight_layout()
-        for region in regions:
-            if region_is_inside_another(region_sizes, region.bbox):
-                continue
-            fig, ax = plt.subplots(figsize=(10,10))
+    plt.tight_layout()
+    for region in regions:
+        if region_is_inside_another(region_sizes, region.bbox):
+            continue
 
-            # take regions with large enough areas
-            if region.area >= 1300 and region.area < (.5*len(binary)*len(binary[0])):
-                # draw rectangle around segmented coins
-                minr, minc, maxr, maxc = region.bbox
-                coin = image2[minr:maxr, minc:maxc]
-                ax.imshow(coin)
-                #rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                #                          fill=False, edgecolor='red', linewidth=2)
+        # take regions with large enough areas
+        if region.area >= 1300 and region.area < (.5*len(binary)*len(binary[0])):
+            fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+            name  =  "fig_{q}_{w}".format(q = fig_name, w = iter)
 
-                minc = 0
-                minr = 0
-                color = 'k'
-                colors = [np.mean(coin[:,:,i]) for i in range(0,3)]
+            # draw rectangle around segmented coins
+            minr, minc, maxr, maxc = region.bbox
+            #rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+            #                          fill=False, edgecolor='red', linewidth=2)
+            coin = image2[minr:maxr, minc:maxc]
+            coin = filters.median()
 
-                name  =  "fig_{q}_{w}".format(q = fig_name, w = iter)
-                text_0= "r:%.2f" %  np.mean(colors[0])
-                text_1  =  "g:%.2f" %  np.mean(colors[1])
-                text_2  =  "b:%.2f" %  np.mean(colors[2])
-                text_3 = " rozrzut: %.2f" % (np.max(colors) - np.min(colors))
-                #text_file.write(name + ";" + text_0 + ";" + text_1 + ";" + text_2+ "\n  ")
-                ax.text(minc, minr - 10, name + " " + text_0 + " " + text_1 + " " + text_2 + " " + text_3, color=color, fontsize=10)
-                #images.append(coin)
-                #ORB_ski(rgb2gray(img_template), rgb2gray(coin))
-                #ax.add_patch(rect)
-                plt.show()
-                #fig.savefig("out/" + name)
-                iter += 1
-                flush_figures()
+            #str = "{n};{rgb};{r};{g};{b};{val}".format(n = name, rgb = np.mean(coin), r = np.mean(coin[:,:,0]), g = np.mean(coin[:,:,1]), b = np.mean(coin[:,:,2]), val = np.mean(rgb2gray(coin)))
+            #log.write(str + "\n")
 
 
-    ax.set_axis_off()
+            continue
+            #coin = filters.median(rgb2gray(coin), np.ones((5,5)))
+            ax.imshow(rgb2gray(coin))
+            plt.show()
+            plt.savefig("out/" + name)
+            iter += 1
+            minc = 0
+            minr = 0
+            color = 'k'
+            break
+            fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+
+            ax.imshow(coin[:,:,1])
+            plt.show()
+            fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+
+            ax.imshow(coin[:,:,2])
+            plt.show()
+            continue
+            colors = [np.mean(coin[:,:,i]) for i in range(0,3)]
+
+            name  =  "fig_{q}_{w}".format(q = fig_name, w = iter)
+            text_0 = "r:%.2f" %  np.mean(colors[0])
+            text_1 =  "g:%.2f" %  np.mean(colors[1])
+            text_2 =  "b:%.2f" %  np.mean(colors[2])
+            text_3 = " rozrzut: %.2f" % (np.max(colors) - np.min(colors))
+            #text_file.write(name + ";" + text_0 + ";" + text_1 + ";" + text_2+ "\n  ")
+            ax.text(minc, minr - 10, name + " " + text_0 + " " + text_1 + " " + text_2 + " " + text_3, color=color, fontsize=10)
+            #images.append(coin)
+            #ax.add_patch(rect)
+            plt.show()
+            #fig.savefig("out/" + name)
+            iter += 1
+            flush_figures()
+
+
+    #ax.set_axis_off()
     #plt.tight_layout()
     # print(contours)
 
-
-    #for list in contours:
-     #   xs, ys = [*zip(*list)]
-        # plt.plot(xs,ys)
-
-
-        #  print(contours)
-   # flush_figures()
 
 
 directory = os.getcwd()+"\\templates" + '/'
@@ -140,21 +147,23 @@ img2 = cv2.drawKeypoints(imag, kp, img3, color=(0,255,0), flags=0)
 plt.imshow(img2)
 plt.show()
 '''
-directory = os.getcwd()+"\\moje_selekcja" + '/'
+directory = os.getcwd()+"\\moje" + '/'
 
-
+log = open("out/figures.csv", "w+")
 for file in os.listdir(directory):
-    #images.append(rgb2gray(img.load(directory + file)))
-    image = img.load(directory + file, True)
-    detector.exec(img_template, histogram_manipulator.contrast_stretching(image[126:899, 138:871]), file)
-    #img = hist.contrast_stretching(img)
-    '''image = img.load(directory+file,True)
+    # images.append(rgb2gray(img.load(directory + file)))
+    #ORB
+    '''
+    #image = img.load(directory + file, True)
+    #detector.exec(img_template, histogram_manipulator.contrast_stretching(image[126:899, 138:871]), file)
+    #img = hist.contrast_stretching(img)'''
+    image = img.load(directory+file,True)
     image2 = img.load(directory+file,False)
     edges = skimage.filters.sobel(image)
     edges = skimage.feature.canny(edges,1.2)
     meanV = getMean("sobel_max_", edges)
     thresh(0.08)
-    fig_name += 1'''
-#ORB_ski(images[0], images[1])
+    fig_name += 1
+log.close()
 #displaySaveImage(images)
 #plt.show()
